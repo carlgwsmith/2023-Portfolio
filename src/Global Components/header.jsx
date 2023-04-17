aa06eb5d5ead064a67204784ba43628820dd21c4
Carl Smith <48308402+carlgwsmith@users.noreply.github.com>
2023-04-16 23:23:18 -0500

    navbar

--- src/Global Components/header.jsx
+++ src/Global Components/header.jsx
@@ -1,17 +1,42 @@
+import { useState } from "react"
+import {HiMenu} from 'react-icons/hi'
+import {AiOutlineClose} from 'react-icons/ai'
+
 function Header(){
+
+const [navbarOpen, setNavbarOpen] = useState(false);
+    
+const handleNav = ()=>{
+    setNavbarOpen(!navbarOpen)
+}
+
+
     return(
-        <div className="bg-white flex border-b-2 p-4 mb-3">
-            <div className="grid grid-rows-1 grid-cols-2 gap-4">
-                <div className="text-xl font-bold tracking-tighter">CGWS</div>
-                <div className="float-right">
-                <div className="grid grid-cols-4 gap-4">
-                    <div>Home</div>
-                    <div>Resume</div>
-                    <div>Portfolio</div>
-                    <div>Contact</div>
+        <div className="flex justify-between items-center max-w-[1240px] bg-white border-b-2 p-4 mb-3 border-gray-900 h-12">
+                <div className="w-full text-xl font-bold tracking-tighter">CGWS</div>
+                <ul className="hidden md:flex">
+                    <li className="p-4">Portfolio</li>
+                    <li className="p-4">Home</li>
+                    <li className="p-4">Contact</li>
+                    <li className="p-4">Resume</li>
+                </ul>
+                <div onClick={handleNav} className='block md:hidden'>
+                    <HiMenu size={20} navBarOpen={true}/>
                 </div>
+                <div className={navbarOpen ? 'fixed left-0 top-0 w-[100%] border-right border-r-gray-900 h-full bg-gray-100 ease-in-out duration-500' : 'fixed left-[-100%]'}>
+                    <div className="grid grid-cols-6">
+                    <div className="col-span-3 text-xl font-bold tracking-tighter m-4 w-[75%] inline-block">CGWS</div>
+                    <div className="col-span-3 p-4 inline-block">
+                <AiOutlineClose size={20} navBarOpen={false} onClick={handleNav} className="float-right"/>
                 </div>
-            </div>
+                    </div>
+                    <ul className="uppercase">
+                        <li className="p-4">Portfolio</li>
+                        <li className="p-4">Home</li>
+                        <li className="p-4">Contact</li>
+                        <li className="p-4">Resume</li>
+                </ul>
+                </div>
         </div>
     )
 }
