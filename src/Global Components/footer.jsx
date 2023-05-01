89753f4103ad110baec9b54a7198c4700f706cbb
Carl Smith <48308402+carlgwsmith@users.noreply.github.com>
2023-04-30 22:53:35 -0500

    Update footer.jsx

--- src/Global Components/footer.jsx
+++ src/Global Components/footer.jsx
@@ -1,25 +1,30 @@
 import {BsDribbble, BsLinkedin, BsGithub} from 'react-icons/bs'
+import {RxDotFilled} from 'react-icons/rx'
 
 export default function Footer(){
     return(
-        <div className="my-10 w-full bg-background-light">
-            <div className="p-10 bg-background-light">
-                <h2 className="text-center font-bold text-3xl text-jet">Check me out elsewhere</h2>
-                <div className="bg-gradient-to-r from-[#FF3C35] via-[#fbb21b] to-[#ff3c35] h-1  animate-bganimate "></div>
-                <div className="grid grid-cols-3 mt-10 text-center">
-                    <div>
-                        <a href="" className='hover:underline'><BsDribbble size={20} style={{display:'inline-block'}}/> Dribbble</a>
-                    </div>
-                    <div>
-                        <a href="" className='hover:underline'><BsLinkedin size={20} style={{display:'inline-block'}}/> LinkedIn</a>
-                    </div>
-                    <div>
-                        <a href="" className='hover:underline'><BsGithub size={20} style={{display:'inline-block'}}/> GitHub</a>
+        <div className="mt-10 pb-4 w-full bg-background-light text-jet">
+            <div className="p-10 bg-background-light grid grid-cols-6">
+                <div className='m-auto col-start-2 col-span-4 pt-5'>
+                    <h2 className="text-center font-bold text-3xl text-jet">Check me out elsewhere</h2>
+                    <div className="bg-gradient-to-r from-[#FF3C35] via-[#fbb21b] to-[#ff3c35] h-1  animate-bganimate "></div>
+                </div>
+                <div className='col-span-4 col-start-2 pb-5'>
+                    <div className="grid grid-cols-3 mt-10 text-center">
+                        <div>
+                            <a href="" className='hover:underline'><BsDribbble size={20} style={{display:'inline-block'}}/> Dribbble</a>
+                        </div>
+                        <div>
+                            <a href="" className='hover:underline'><BsLinkedin size={20} style={{display:'inline-block'}}/> LinkedIn</a>
+                        </div>
+                        <div>
+                            <a href="" className='hover:underline'><BsGithub size={20} style={{display:'inline-block'}}/> GitHub</a>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className='text-center bg-[#fff] w-full p-4 border-y-[2px] border-solid border-jet'>
-                &copy; 2023 Carl G.W. Smith 
+            <div className='text-center bg-[#fff] w-full p-4 border-y-[2px] border-solid border-jet text-sm font-semibold text-jet'>
+                &copy; 2023 <RxDotFilled size={14} className="inline-block"/> Carl G.W. Smith 
             </div>
         </div>
     )
