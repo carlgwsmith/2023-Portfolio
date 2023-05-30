c2f22579c637c6381eea6ab44c4dc4df915f39ab
Carl Smith <48308402+carlgwsmith@users.noreply.github.com>
2023-05-29 22:40:33 -0500

    css fixes

--- src/Case Studies/CaseStudyDescBar.jsx
+++ src/Case Studies/CaseStudyDescBar.jsx
@@ -1,6 +1,6 @@
 export default function CaseStudyDescBar(props){
     return(
-        <div className="grid grid-cols-3 text-center text-jet m-auto justify-center">
+        <div className="grid grid-cols-3 text-center text-jet m-auto justify-center my-6">
             <div className="p-4 justify-center justify-self-center my-auto">
                 <p className="font-bold">Year:</p>
                 {props.year}
--- src/Case Studies/Objectives.jsx
+++ src/Case Studies/Objectives.jsx
@@ -2,19 +2,19 @@ import {BiChevronRight} from 'react-icons/bi'
 
 export default function Objectives(){
     return(
-        <div className="grid grid-cols-2 text-jet bg-background-light py-5 px-10 gap-4">
+        <div className="grid grid-cols-2 text-jet bg-background-light py-10 px-10 gap-4">
             <div className="mt-4">
                 <h2 className="text-center text-xl font-semibold">Objectives & Goals</h2>
                 <ol className="list-decimal mt-4">
-                    <li>Provide an easy-to-use platform for users to search and compare life insurance products offered by different agents.</li>
-                    <li>Enable users to easily contact licensed life insurance agents who can provide personalized guidance and advice on choosing the right policy.</li>
-                    <li>Provide a forum or community for users to share their experiences with different life insurance providers and agents, and to ask questions or seek advice from other users.</li>
-                    <li>Provide a secure platform for users to submit their personal information and receive quotes from different insurance companies.</li>
+                    <li className='mb-4'>Provide an easy-to-use platform for users to search and compare life insurance products offered by different agents.</li>
+                    <li className='mb-4'>Enable users to easily contact licensed life insurance agents who can provide personalized guidance and advice on choosing the right policy.</li>
+                    <li className='mb-4'>Provide a forum or community for users to share their experiences with different life insurance providers and agents, and to ask questions or seek advice from other users.</li>
+                    <li className='mb-4'>Provide a secure platform for users to submit their personal information and receive quotes from different insurance companies.</li>
                 </ol>
             </div>
             <div className="mt-4">
             <h2 className="text-center text-xl font-semibold">Business Challenges</h2>
-            <ul className='mt-4 text-md font-medium'>
+            <ul className='mt-4 text-md font-medium ml-10'>
                 <li className='mb-4'><BiChevronRight className='inline -mt-1' size="22px"/> Competition</li>
                 <li className='mb-4'><BiChevronRight className='inline -mt-1' size="22px"/> Building Trust</li>
                 <li className='mb-4'><BiChevronRight className='inline -mt-1' size="22px"/> Regulatory Compliance</li>
