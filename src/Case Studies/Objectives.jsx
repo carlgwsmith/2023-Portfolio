566ab7e8b28ef8e877a7dd185e2f4c5255b98908
Carl Smith <48308402+carlgwsmith@users.noreply.github.com>
2023-05-29 22:45:43 -0500

    css fixes

--- src/Case Studies/Objectives.jsx
+++ src/Case Studies/Objectives.jsx
@@ -2,10 +2,10 @@ import {BiChevronRight} from 'react-icons/bi'
 
 export default function Objectives(){
     return(
-        <div className="grid grid-cols-2 text-jet bg-background-light py-10 px-10 gap-4">
+        <div className="grid grid-cols-2 text-jet bg-background-light py-10 px-10 gap-20">
             <div className="mt-4">
                 <h2 className="text-center text-xl font-semibold">Objectives & Goals</h2>
-                <ol className="list-decimal mt-4">
+                <ol className="list-decimal mt-4 ml-10 text-sm">
                     <li className='mb-4'>Provide an easy-to-use platform for users to search and compare life insurance products offered by different agents.</li>
                     <li className='mb-4'>Enable users to easily contact licensed life insurance agents who can provide personalized guidance and advice on choosing the right policy.</li>
                     <li className='mb-4'>Provide a forum or community for users to share their experiences with different life insurance providers and agents, and to ask questions or seek advice from other users.</li>
@@ -14,7 +14,7 @@ export default function Objectives(){
             </div>
             <div className="mt-4">
             <h2 className="text-center text-xl font-semibold">Business Challenges</h2>
-            <ul className='mt-4 text-md font-medium ml-10'>
+            <ul className='mt-4 text-md font-medium ml-20'>
                 <li className='mb-4'><BiChevronRight className='inline -mt-1' size="22px"/> Competition</li>
                 <li className='mb-4'><BiChevronRight className='inline -mt-1' size="22px"/> Building Trust</li>
                 <li className='mb-4'><BiChevronRight className='inline -mt-1' size="22px"/> Regulatory Compliance</li>
--- src/Case Studies/Process.jsx
+++ src/Case Studies/Process.jsx
@@ -2,10 +2,10 @@ import {BiSearchAlt, BiBulb, BiVector, BiRepeat, BiCodeAlt} from 'react-icons/bi
 export default function Process(){
 
     const roundIcon = 'border-solid border-2 border-jet rounded-full p-3 w-fit drop-shadow-md bg-white m-auto'
-    const iconName = 'font-medium my-2'
+    const iconName = 'font-medium mt-2'
 
     return(
-        <div className="p-10 grid grid-cols-6 text-jet">
+        <div className="px-10 py-6 grid grid-cols-6 text-jet">
         <div className='m-auto col-start-2 col-span-4 pt-5 pb-5'>
             <h2 className="text-center font-bold text-3xl text-jet">Process</h2>
             <div className="bg-gradient-to-r from-[#FF3C35] via-[#fbb21b] to-[#ff3c35] h-1  animate-bganimate "></div>
