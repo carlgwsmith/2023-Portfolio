62dff2355514579ec2e04d7b1115785402be8b58
Carl Smith <48308402+carlgwsmith@users.noreply.github.com>
2023-05-29 22:56:17 -0500

    Update Research.jsx

--- src/Case Studies/Research.jsx
+++ src/Case Studies/Research.jsx
@@ -1,7 +1,7 @@
 export default function Research(){
     return(
-        <div>
-            <div className="px-10 pt-6 grid grid-cols-6 text-jet">
+        <div className="bg-background-light p-10 pt-6 pb-10 text-jet">
+            <div className="grid grid-cols-6">
                 <div className='m-auto col-start-2 col-span-4 pt-5 pb-5'>
                     <h2 className="text-center font-bold text-3xl text-jet mb-3">Research</h2>
                     <div className="bg-gradient-to-r from-[#FF3C35] via-[#fbb21b] to-[#ff3c35] h-1  animate-bganimate "></div>
@@ -9,12 +9,9 @@ export default function Research(){
             </div>
             <div className="grid grid-cols-6">
                 <div className="col-start-2 col-span-4">
-            <p>We began the research process by conducting user research. We looked up potential users of the tool to understand their needs and pain points. We also analyzed data from existing life insurance websites to get a sense of the market.</p>
-
-            <p className="mt-5">Based on our research, we developed a set of user personas. These personas represent the different types of users who would use the tool. We used the personas to guide our design decisions.</p>
-
-            <p className="mt-5">We then created wireframes and prototypes of the tool.</p>
-
+                    <p>We began the research process by conducting user research. We looked up potential users of the tool to understand their needs and pain points. We also analyzed data from existing life insurance websites to get a sense of the market.</p>
+                    <p className="mt-5">Based on our research, we developed a set of user personas. These personas represent the different types of users who would use the tool. We used the personas to guide our design decisions.</p>
+                    <p className="mt-5">We then created wireframes and prototypes of the tool.</p>
                 </div>
             </div>
 
