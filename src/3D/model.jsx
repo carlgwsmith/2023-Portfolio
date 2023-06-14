62ae9b80be0d3693194b23aeb553a894903d7faa
Carl Smith <48308402+carlgwsmith@users.noreply.github.com>
2023-06-13 23:36:31 -0500

    some weird responsizeness that i need to fix later

--- src/3D/model.jsx
+++ src/3D/model.jsx
@@ -12,7 +12,7 @@ export default function Model(){
     })
     return (
     <>
-    <primitive object={model.scene} position={[-1,-.5,-1]} scale={.4} rotation-y={4.7} rotation-z={0.02} rotation-x={0}/>
+    <primitive object={model.scene} position={[-1,-.5,-1]} scale={.4} rotation-y={4.7} rotation-z={0.02} rotation-x={0} className="model"/>
     <Sparkles size={6} scale={4, 2, 5} position-y={1.5} speed={.5} count={150} color={'tan'}/>
     </>
     )
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,11 +3,39 @@ import FeaturedWork from "./FeaturedWork"
 import Scrolling from "./Scrolling"
 import Experience from "../3D/Environment"
 import { Canvas } from "@react-three/fiber"
+import { useRef, useEffect, useState } from 'react'
+
 export default function Home(){
+    const [expWidth, setExpWidth] = useState('800px')
+    const [expHeight, setExpHeight] = useState('600px')
+    
+    const windowSize = useRef([window.innerWidth, window.innerHeight]);
+
+    useEffect(() => {
+        console.log(windowSize.current[0])
+        if(windowSize.current[0] <= 500){
+            console.log('smaller')
+            setExpWidth('400px')
+            setExpHeight('400px')
+        }
+        else if(windowSize.current[0] < 800){
+            setExpWidth('500px')
+            setExpHeight('300px')
+        }
+        else if(windowSize.current[0] < 1400){
+            setExpWidth('600px')
+            setExpHeight('400px')
+            console.log('small')
+        }
+         else{
+            setExpWidth('800px')
+            setExpHeight('600px')
+        }
+    }, [windowSize]);
     return(
         <>
         <HeroHome title="Carl G.W. Smith" desc="I’m Carl, a senior product designer and creative developer.
-I design and implement thoughtful and easy to use experiences for companies with complex problems." rightSide={<Canvas style={{width: '800px', height: '600px'}} camera={{
+I design and implement thoughtful and easy to use experiences for companies with complex problems." rightSide={<Canvas style={{width: expWidth, height: expHeight, overflow:'hidden'}} camera={{
     fov: 60,
     zoom: 1.45,
     near: 0.1,
