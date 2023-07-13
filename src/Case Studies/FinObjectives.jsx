5b9e8353bc918a29e63ac520fec3b6d0ea770f77
Carl Smith <48308402+carlgwsmith@users.noreply.github.com>
2023-07-12 23:19:14 -0500

    finary

---
+++ src/Case Studies/FinObjectives.jsx
@@ -0,0 +1,28 @@
+import {BiChevronRight} from 'react-icons/bi'
+
+export default function Objectives(){
+    return(
+        <div className="grid grid-cols-6 text-jet bg-background-light py-10 px-10 gap-10">
+            <div className="mt-4 md:col-span-3 sm:col-span-6 col-span-6">
+                <h2 className="text-center text-xl font-semibold">Objectives & Goals</h2>
+                <ol className="list-decimal mt-4 ml-10 text-sm">
+                    <li className='mb-4'>Improve customer service by providing financial advisors with a centralized system for managing customer interactions, storing customer data, and tracking customer satisfaction.</li>
+                    <li className='mb-4'>Customer relationship management. The tool should allow financial advisors to store customer data, track customer interactions, and manage customer relationships.</li>
+                    <li className='mb-4'>Reporting. The tool should allow financial advisors to track and measure their progress, and to gain insights into their business.</li>
+                    <li className='mb-4'>Lead management. The tool should allow financial advisors to track leads, nurture them, and close deals.</li>
+                </ol>
+            </div>
+            <div className="mt-4 md:col-span-3 sm:col-span-6 col-span-6">
+            <h2 className="text-center text-xl font-semibold">Business Challenges</h2>
+            <ul className='mt-4 text-md font-medium ml-20'>
+                <li className='mb-4'><BiChevronRight className='inline -mt-1' size="22px"/> Attracting & Retaining Clients</li>
+                <li className='mb-4'><BiChevronRight className='inline -mt-1' size="22px"/> Managing Risk</li>
+                <li className='mb-4'><BiChevronRight className='inline -mt-1' size="22px"/> Regulatory Compliance</li>
+                <li className='mb-4'><BiChevronRight className='inline -mt-1' size="22px"/> Complex Data Structures</li>
+                <li className='mb-4'><BiChevronRight className='inline -mt-1' size="22px"/> Keeping up with Industry Changes</li>
+            </ul>
+            </div>
+
+        </div>
+    )
+}
--- src/Case Studies/Finary.jsx
+++ src/Case Studies/Finary.jsx
@@ -1,11 +1,15 @@
 import Hero from '../Home/Hero'
 import HeroImg from '../assets/finary-hero.png'
 import CaseStudyDescBar from './CaseStudyDescBar'
+import Objectives from './FinObjectives'
+import Process from './Process'
 export default function Finary (){
     return(
         <div>
             <Hero title="Finary CRM" desc="This application assists financial advisors and representatives in tracking customer interactions, managing data, engagement and performance of their financial portfolio.  The tool is intended to improve customer service and increase an advisors book of business." rightSide={HeroImg} image='true' />
             <CaseStudyDescBar year="2023" role="Product Designer, Developer" duration="4 mos"/>
+            <Objectives/>
+            <Process/>
         </div>
     )
 }
