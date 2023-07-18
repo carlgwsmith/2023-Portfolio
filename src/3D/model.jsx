d962e9baf183838068021abe1b57e0da335a0d55
Carl Smith <48308402+carlgwsmith@users.noreply.github.com>
2023-07-17 23:11:39 -0500

    Update model.jsx

--- src/3D/model.jsx
+++ src/3D/model.jsx
@@ -1,7 +1,7 @@
 import { useLoader, useFrame } from "@react-three/fiber"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js"
-import { Sparkles, CameraShake } from '@react-three/drei'
+import { Sparkles, CameraShake, OrbitControls, Html, Text } from '@react-three/drei'
 import { useEffect, useRef } from 'react'
 export default function Model(){
     const modelprim = useRef()
@@ -17,8 +17,12 @@ export default function Model(){
         // modelprim.current.position.x=1 +Math.cos(time/4)
         // modelprim.current.rotation.y +=(delta * 0.2)
     })
+    
+    const play = 'w-[100px] p-3 bg-white text-jet bg-opacity-60 rounded'
     return (
     <>
+    <Html className={play}>DRAG ME!</Html>
+     <OrbitControls makeDefault />
     <primitive ref={modelprim} object={model.scene} position={[-1.2,-.5,-1]} scale={.4} rotation-y={4.7} rotation-z={0.02} rotation-x={0} className="model"/>
     </>
     )
