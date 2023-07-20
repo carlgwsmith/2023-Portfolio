570f2674ed1eb8cb4660bd5752759b76d03337e8
Carl Smith <48308402+carlgwsmith@users.noreply.github.com>
2023-07-19 22:50:59 -0500

    iptiq

--- src/Case Studies/IptiQ.jsx
+++ src/Case Studies/IptiQ.jsx
@@ -1,7 +1,18 @@
+import Hero from '../Home/Hero'
+import HeroImg from '../IptiqAssets/imaciptiq.png'
+import CaseStudyDescBar from './CaseStudyDescBar'
+import Objectives from './IptiqObjectives'
+import Process from './Process'
+import Research from './Research'
+
 export default function IptiQ (){
     return(
         <div>
-            iptiq
+           <Hero title="IptiQ E-App" desc="The IptiQ Electronic insurance applications are designed to make the process of applying for insurance easier and more efficient for both consumers and insurance companies. They allow consumers to complete their applications online, at their convenience, and without having to mail in any paperwork saving consumers time, money, and it can also help to reduce the risk of errors in the application process." rightSide={HeroImg} image='true'/>
+            <CaseStudyDescBar year="2022" role="Product Designer, Developer" duration="1 Yr"/>
+            <Objectives/>
+            <Process/>
+            <Research content={<div><p className='mb-10'>The need for an internal e-application came from the need to remove reliance on a third party, improve user experience, and be more agile with new features while growing business. A deep analysis of the previous e-application was taken and critiqued. External analysis of competitor products were conducted by our competitive audit team. </p></div>}/>
         </div>
     )
 }
---
+++ src/Case Studies/IptiqObjectives.jsx
@@ -0,0 +1,29 @@
+import {BiChevronRight} from 'react-icons/bi'
+
+export default function IptiqObjectives(){
+    return(
+        <div className="grid grid-cols-6 text-jet bg-background-light py-10 px-10 gap-10">
+            <div className="mt-4 md:col-span-3 sm:col-span-6 col-span-6">
+                <h2 className="text-center text-xl font-semibold">Objectives & Goals</h2>
+                <ol className="list-decimal mt-4 ml-10 text-sm">
+                    <li className='mb-4'>Improve customer service by providing financial advisors with a centralized system for managing customer interactions, storing customer data, and tracking customers.</li>
+                    <li className='mb-4'>Provide more convenience for clients by allowing them to complete their applications online, at your their own pace and time.</li>
+                    <li className='mb-4'>Reduce inaccuracies that are often found within traditional paper application through the use of validation.</li>
+                    <li className='mb-4'>Offer more security to protect private identifiable information for clients and agents by reducing third parties.</li>
+                    <li className='mb-4'>Reduce paper and third party costs by bringing digital applications in-house</li>
+                </ol>
+            </div>
+            <div className="mt-4 md:col-span-3 sm:col-span-6 col-span-6">
+            <h2 className="text-center text-xl font-semibold">Business Challenges</h2>
+            <ul className='mt-4 text-md font-medium ml-20'>
+                <li className='mb-4'><BiChevronRight className='inline -mt-1' size="22px"/> Security</li>
+                <li className='mb-4'><BiChevronRight className='inline -mt-1' size="22px"/> Usability</li>
+                <li className='mb-4'><BiChevronRight className='inline -mt-1' size="22px"/> Regulatory Compliance</li>
+                <li className='mb-4'><BiChevronRight className='inline -mt-1' size="22px"/> Business Ownership/Integration</li>
+                <li className='mb-4'><BiChevronRight className='inline -mt-1' size="22px"/> Cost</li>
+            </ul>
+            </div>
+
+        </div>
+    )
+}
